35033c5db117efce018a0ae12aa8fcd55a27334e
AlbertWebs <albertmuhatia@gmail.com>
2021-11-10 17:47:24 +0300

    fixe autoplay

--- resources/views/front/website/slider.blade.php
+++ resources/views/front/website/slider.blade.php
@@ -1,4 +1,4 @@
-<div class="owl-carousel owl-carousel-light owl-carousel-light-init-fadeIn owl-theme manual dots-inside dots-horizontal-center dots-light show-dots-hover nav-inside nav-inside-plus nav-dark nav-md nav-font-size-md show-nav-hover mb-0" data-plugin-options="{'autoplayTimeout': 7000}" data-dynamic-height="['670px','670px','670px','550px','500px']" style="height: 670px;">
+<div class="owl-carousel owl-carousel-light owl-carousel-light-init-fadeIn owl-theme manual dots-inside dots-horizontal-center dots-light show-dots-hover nav-inside nav-inside-plus nav-dark nav-md nav-font-size-md show-nav-hover mb-0" data-plugin-options="{'autoplayTimeout': 7000, 'autoplay': false}" data-dynamic-height="['670px','670px','670px','550px','500px']" style="height: 670px;">
     <div class="owl-stage-outer">
         <div class="owl-stage">
 
