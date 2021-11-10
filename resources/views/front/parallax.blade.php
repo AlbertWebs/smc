e88fc3a5a420513cc42182652fa9ed166d6ea5e9
AlbertWebs <albertmuhatia@gmail.com>
2021-11-10 18:30:16 +0300

    Fix parallax

--- resources/views/front/parallax.blade.php
+++ resources/views/front/parallax.blade.php
@@ -428,7 +428,7 @@
 								<span class="thumb-info thumb-info-hide-wrapper-bg bg-transparent border-radius-0">
 									<span class="thumb-info-wrapper border-radius-0">
 										<a href="about-me.html">
-											<img src="{{asset('theme/img/team/team-2.jpg')}}" class="img-fluid border-radius-0" alt="">
+											<img src="{{asset('theme/img/team/team-1.jpg')}}" class="img-fluid border-radius-0" alt="">
 											<span class="thumb-info-title">
 												<span class="thumb-info-inner">Jessica Doe</span>
 												<span class="thumb-info-type">Marketing</span>
@@ -449,7 +449,7 @@
 								<span class="thumb-info thumb-info-hide-wrapper-bg bg-transparent border-radius-0">
 									<span class="thumb-info-wrapper border-radius-0">
 										<a href="about-me.html">
-											<img src="{{asset('theme/img/team/team-3.jpg')}}" class="img-fluid border-radius-0" alt="">
+											<img src="{{asset('theme/img/team/team-1.jpg')}}" class="img-fluid border-radius-0" alt="">
 											<span class="thumb-info-title">
 												<span class="thumb-info-inner">Rick Edward Doe</span>
 												<span class="thumb-info-type">Developer</span>
@@ -470,7 +470,7 @@
 								<span class="thumb-info thumb-info-hide-wrapper-bg bg-transparent border-radius-0">
 									<span class="thumb-info-wrapper border-radius-0">
 										<a href="about-me.html">
-											<img src="{{asset('theme/img/team/team-4.jpg')}}" class="img-fluid border-radius-0" alt="">
+											<img src="{{asset('theme/img/team/team-1.jpg')}}" class="img-fluid border-radius-0" alt="">
 											<span class="thumb-info-title">
 												<span class="thumb-info-inner">Melinda Wolosky</span>
 												<span class="thumb-info-type">Design</span>
@@ -512,7 +512,7 @@
 								<span class="thumb-info thumb-info-hide-wrapper-bg bg-transparent border-radius-0">
 									<span class="thumb-info-wrapper border-radius-0">
 										<a href="about-me.html">
-											<img src="{{asset('theme/img/team/team-2.jpg')}}" class="img-fluid border-radius-0" alt="">
+											<img src="{{asset('theme/img/team/team-1.jpg')}}" class="img-fluid border-radius-0" alt="">
 											<span class="thumb-info-title">
 												<span class="thumb-info-inner">Jessica Doe</span>
 												<span class="thumb-info-type">Marketing</span>
@@ -533,7 +533,7 @@
 								<span class="thumb-info thumb-info-hide-wrapper-bg bg-transparent border-radius-0">
 									<span class="thumb-info-wrapper border-radius-0">
 										<a href="about-me.html">
-											<img src="{{asset('theme/img/team/team-3.jpg')}}" class="img-fluid border-radius-0" alt="">
+											<img src="{{asset('theme/img/team/team-1.jpg')}}" class="img-fluid border-radius-0" alt="">
 											<span class="thumb-info-title">
 												<span class="thumb-info-inner">Rick Edward Doe</span>
 												<span class="thumb-info-type">Developer</span>
@@ -554,7 +554,7 @@
 								<span class="thumb-info thumb-info-hide-wrapper-bg bg-transparent border-radius-0">
 									<span class="thumb-info-wrapper border-radius-0">
 										<a href="about-me.html">
-											<img src="{{asset('theme/img/team/team-4.jpg')}}" class="img-fluid border-radius-0" alt="">
+											<img src="{{asset('theme/img/team/team-1.jpg')}}" class="img-fluid border-radius-0" alt="">
 											<span class="thumb-info-title">
 												<span class="thumb-info-inner">Melinda Wolosky</span>
 												<span class="thumb-info-type">Design</span>
