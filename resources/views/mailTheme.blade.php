5a9904d8666f2d31631ca5a535f7d0adfa36cd7a
AlbertWebs <albertmuhatia@gmail.com>
2022-07-12 17:21:37 +0300

    Mail Theme Logo

--- resources/views/mailTheme.blade.php
+++ resources/views/mailTheme.blade.php
@@ -121,7 +121,7 @@
                     <tr>
                         <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
                             <!-- <h1 style="font-size: 48px; font-weight: 400; margin: 2;">Hello Albert,</h1>  -->
-                            <img src="{{url('/')}}/uploads/logo/logo.png" width="225"  style="display: block; border: 0px;" />
+                            <img src="https://sasemagroup.com/uploads/logo/SASEMA-LOGO-04.png" width="225"  style="display: block; border: 0px;" />
                         </td>
                     </tr>
                 </table>
